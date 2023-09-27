dd435af479060923600998b8f732563e9a0c1954
CoderAltair <kamronqodirov441gmail.com>
2023-09-27 18:08:28 +0500

    sfdf

---
+++ android/app/src/main/kotlin/com/example/sajda/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sajda
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/bloc_state_manegment/savedSuraBloc/get_sura_name_with_isar_bloc.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sajda/models/isar_sura/user.dart';
+import 'package:sajda/services/iser_service/isar_service.dart';
+part 'get_sura_name_with_isar_event.dart';
+part 'get_sura_name_with_isar_state.dart';
+
+class GetSuraNameWithIsarBloc
+    extends Bloc<GetSuraNameWithIsarEvent, GetSuraNameWithIsarState> {
+  GetSuraNameWithIsarBloc() : super(GetSuraNameWithIsarInitial()) {
+    on<StartSuraNameWithIsarEvent>(getIsarSuraNameAndIndex);
+    on<RemoveSuraNameWithIsarEvent>(removeIsarSuraNameAndIndex);
+  }
+
+  Future<void> getIsarSuraNameAndIndex(
+    StartSuraNameWithIsarEvent event,
+    Emitter<GetSuraNameWithIsarState> emmit,
+  ) async {
+    List<User> newUser = await IsarService().getAllSura();
+    emmit(StartSuraNameWithIsarState(newUser));
+  }
+
+  Future<void> removeIsarSuraNameAndIndex(
+    RemoveSuraNameWithIsarEvent event,
+    Emitter<GetSuraNameWithIsarState> emmit,
+  ) async {
+    await IsarService().removeSura(event.id);
+    emmit(ReloadingSuraNameWithIsarState());
+  }
+}
